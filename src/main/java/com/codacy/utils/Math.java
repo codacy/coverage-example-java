8c5a449b6cbe9d3132f2b270e86befeccdba27da
Rúben Santos <ruben.santos@codacy.com>
2022-05-18 11:15:29 +0100

    Add lines withotu coverage

--- src/main/java/com/codacy/utils/Math.java
+++ src/main/java/com/codacy/utils/Math.java
@@ -12,10 +12,14 @@ public class Math {
      * Adds 2 numbers, unless there is a magic number on the second argument
      */
     public int magicAdd(int x, int y) {
+        System.out.println(x);
+        System.out.println(y);
         if (y == magicNumber) {
+            System.out.println(x);
             return y - x;
         } else {
-	    System.out.println("hey");
+            System.out.println("hey");
+            System.out.println(y);
             return x + y;
         }
     }
