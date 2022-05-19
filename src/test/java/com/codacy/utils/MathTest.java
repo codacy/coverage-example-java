d3b239aa9cafd7dcbf3acb34838f78cf39045906
Rúben Santos <ruben.santos@codacy.com>
2022-05-19 11:38:17 +0100

    remove test

--- src/test/java/com/codacy/utils/MathTest.java
+++ src/test/java/com/codacy/utils/MathTest.java
@@ -5,17 +5,17 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class MathTest {
-    @Test
-    public void shouldAddNumbers() {
-        Math math = new Math(23);
+    // @Test
+    // public void shouldAddNumbers() {
+    // Math math = new Math(23);
 
-        assertEquals(7, math.magicAdd(3, 4));
-    }
+    // assertEquals(7, math.magicAdd(3, 4));
+    // }
 
     // Uncomment this to have 100% coverage
-//    @Test
-//    public void shouldSubtractIfMagicNumber() {
-//        Math math = new Math(4);
-//        assertEquals(1, math.magicAdd(3, 4));
-//    }
+    // @Test
+    // public void shouldSubtractIfMagicNumber() {
+    // Math math = new Math(4);
+    // assertEquals(1, math.magicAdd(3, 4));
+    // }
 }
