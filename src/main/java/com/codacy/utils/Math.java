7b67d78a393a51fe3de24eefd69872570770a113
Miguel Marcelino <mac.marcelino@outlook.pt>
2023-10-06 11:57:30 +0100

    Test coverage in dev

--- src/main/java/com/codacy/utils/Math.java
+++ src/main/java/com/codacy/utils/Math.java
@@ -16,10 +16,12 @@ public class Math {
             System.out.println("Am I am covered?");
             System.out.println("Am I am covered?");
             System.out.println("Am I am covered?");
+            System.out.println("Covered, am I?");
             return y - x;
         } else {
             System.out.println("I am covered!");
             System.out.println("I am covered!");
+            System.out.println("Covered, I am!");
             return x + y;
         }
     }
